75d41952c5039a414c1958c4484ec7c550508ae5
Crowdbotics <team@crowdbotics.com>
2023-06-19 20:44:11 +0000

    Initial commit.

---
+++ backend/violett_41607/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for violett_41607 project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'violett_41607.settings')
+
+application = get_wsgi_application()
---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "violett-41607",
+  url: "https://violett-41607.botics.co",
+  api: "https://violett-41607.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
